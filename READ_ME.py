33b9c63783e40183c8fae84dddd182a15d31c110
Sheridan Vaughn Janda <sheridan.v.janda33@gmail.com>
2023-05-30 13:06:50 -0700

    completed

---
+++ READ_ME.py
@@ -0,0 +1,30 @@
+# create a function call ROI
+
+# class income
+    # input function to ask for income
+
+# class expenses
+    # input for yearly tax % 12 150
+    # input for utilities 100
+    # input for HOA 40
+    # input for repairs 200
+    # input for property management fee 10%
+    # mortgage applicable to tax or not 70%
+
+# class cash flow
+    # income - expenses
+
+# class for C on C ROI
+    #initial investment 
+        #down payment 5%
+        #closing costs 8,000
+        #repair budget 22,000
+    
+    #total investment
+        # cash flow * 12 / total investment
+            #percentage value
+
+
+
+def calculate_return_on_investment(client_name, income, expenses, cash_flow )
+    
